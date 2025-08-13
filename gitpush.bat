1764137fdaaffac1d486bb46f509eff37a4cbc39
董 <996515325@qq.com>
2025-08-13 16:42:14 +0800

    update

--- gitpush.bat
+++ gitpush.bat
@@ -7,9 +7,9 @@ set "logfile=%~dp0deploy_log.txt"
 set msg=%1
 if "%msg%"=="" set msg=update
 
-call hexo g >> "%logfile%" 2>&1
-call hexo d >> "%logfile%" 2>&1
+call hexo g 
+call hexo d 
 
-git add . >> "%logfile%" 2>&1
-git commit -m "%msg%" >> "%logfile%" 2>&1
-git push all source >> "%logfile%" 2>&1
+git add . 
+git commit -m "%msg%" 
+git push all source
