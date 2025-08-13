3952c13c2ba7624730873cb3d4d6971cabb60e86
董 <996515325@qq.com>
2025-08-13 16:14:37 +0800

    update

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 set msg=%1
 if "%msg%"=="" set msg=update
 
-git add .
-git commit -m "%msg%"
-git push all source
+REM 出错就退出
+call :run hexo g
+call :run hexo d
+call :run git add .
+call :run git commit -m "%msg%"
+call :run git push all source
+exit /b
+
+:run
+%*
+if errorlevel 1 (
+    echo 命令执行失败: %*
+    exit /b %errorlevel%
+)
+exit /b
